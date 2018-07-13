55b494785141574f03d54f1d2f6e604512124fb4
cymurs <myfuture2004@163.com>
2018-07-13 18:17:13 +0800

    添加自定义线程控件和消息队列

---
+++ QtCreatorProject/ctextedit.cpp
@@ -0,0 +1,6 @@
+#include "ctextedit.h"
+
+CTextEdit::CTextEdit()
+{
+
+}
---
+++ QtCreatorProject/ctextedit.h
@@ -0,0 +1,16 @@
+#ifndef CTEXTEDIT_H
+#define CTEXTEDIT_H
+
+
+class CTextEdit : public QDeclarativeItem
+{
+    Q_OBJECT
+public:
+    CTextEdit();
+
+signals:
+
+public slots:
+};
+
+#endif // CTEXTEDIT_H
